2f4dcb89cd3fe86267b7e060b2c5a0e461d84c89
bayuadiwibrata <32184425+bayuadiwibrata@users.noreply.github.com>
2024-04-16 11:45:52 +0700

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  project = ""
-  region  = "us-west1"
-  zone    = "us-west1-a"
+  project = "devops-dev-0109"
+  region  = "asia-southeast1"
+  zone    = "asia-southeast1-b"
 }
